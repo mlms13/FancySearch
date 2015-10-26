a91307ccfea0650e479dc96c7a7a61390a8b5988
Michael Martin-Smucker <mlmartin13@gmail.com>
2015-10-26 13:24:05 -0600

    Generate documentation with new structure

--- docs/ImportFancy.hx
+++
@@ -1,5 +0,0 @@
-import fancy.util.Dom;
-import fancy.util.Keys;
-import fancy.util.Types;
-import fancy.Search;
-import fancy.Suggestions;
---
+++ docs/ImportFancySearch.hx
@@ -0,0 +1,5 @@
+import fancy.search.util.Dom;
+import fancy.search.util.Keys;
+import fancy.search.util.Types;
+import fancy.search.Suggestions;
+import fancy.Search;
